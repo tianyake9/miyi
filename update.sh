2f08235734a250d31e4ae01cba0bdfb926e0209f
tianyake <2415407354@qq.com>
2026-05-16 10:58:24 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,15 @@
 
+#!/bin/bash
+set -e
+
+GEOIP_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/latest/download/geoip.dat"
+GEOSITE_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/latest/download/geosite.dat"
+
+SUB_URL="https://miyi-71l.pages.dev/sub?token=cc7169dfdfbe6a11c987d1d9ce8f92af&target=clash"
+SUB_FILE="nodes.yaml"
+
+echo "🔄 开始自动更新..."
+curl -fsSL $GEOIP_URL -o geoip.dat
+curl -fsSL $GEOSITE_URL -o geosite.dat
+curl -fsSL $SUB_URL -o $SUB_FILE
+echo "✅ 更新完成"
